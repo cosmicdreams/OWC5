68510cc170c930c4a63e767ac5c1a212f0387c57
William Jaspers <willjaspers4@gmail.com>
2013-03-10 06:47:15 -0500

    Add template for upcoming events.

---
+++ sites/all/themes/drupalrocks/templates/views-view-list--upcoming-events.tpl.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * - $title : The title of this group of rows.  May be empty.
+ * - $options['type'] will either be ul or ol.
+ * @ingroup views_templates
+ */
+?>
+<?php print $wrapper_prefix; ?>
+  <?php if (!empty($title)) : ?>
+    <?php /** THIS IS A BUG. 
+       The core date type handler doesn't allow us to specify two date formats that would otherwise be case sensitively different.
+       As such, we've created a date format that includes doublequotes.
+       This will strip them. */ ?>
+    <h3><?php print str_replace('"', $title); ?></h3>
+  <?php endif; ?>
+  <?php print $list_type_prefix; ?>
+    <?php foreach ($rows as $id => $row): ?>
+      <li class="<?php print $classes_array[$id]; ?>"><?php print $row; ?></li>
+    <?php endforeach; ?>
+  <?php print $list_type_suffix; ?>
+<?php print $wrapper_suffix; ?>
