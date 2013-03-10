9034c46cbb366eb93116e5afc8805c49b2f7f18d
Noah Blon <noahblon@gmail.com>
2013-03-09 18:41:40 -0600

    Stylin

--- sites/all/themes/drupalrocks/js/script.js
+++ sites/all/themes/drupalrocks/js/script.js
@@ -13,7 +13,47 @@
 (function ($, Drupal, window, document, undefined) {
 
 	$(function(){
-		alert('hi!');
+		$('#page').before('<div style="color:white;background-color:black;padding:10px 15px;position:fixed;left:0;bottom:0">Script loaded</div>')
+		//$('.form-select').hide();
+		
+		$('select').each(function(){
+			var id = $(this).attr('id');
+			var optionSelected = $(this).find(':selected').text();
+			$(this).css('background-color','blue');
+			$(this).after('<ul class="styledSelect_list styledSelect_list-'+id+' styledSelect_list-isHidden"></ul>');
+			$(this).after('<div class="styledSelect_optionSelected styledSelect_optionSelected-'+id+'"><span class="styledSelect_optionText">'+optionSelected+'<span></div>');
+
+
+			$(this).find('option').each(function(){
+				var val = $(this).text();
+				$('.styledSelect_list-'+id+'').append('<li class="styledSelect_option">'+val+'</li>');
+			});
+		});
+		$('.styledSelect_optionSelected').click(function(){
+			$(this).next('.styledSelect_list').toggleClass('styledSelect_list-isHidden');
+		});
+
+		$('.styledSelect_option').click(function(){
+			var text = $(this).text();
+			var parentList = $(this).parent()
+			var parentOptionSelected = parentList.prev();
+			var parentSelect = parentOptionSelected.prev();
+			$(parentList).toggleClass('styledSelect_list-isHidden');
+			parentOptionSelected.text(text);
+			$(parentSelect).find('option').filter(function() {
+			    return $(this).text() == text; 
+			}).attr('selected', true);
+
+		});
+
+		/*
+		$('select').each(function(){
+			var index = $(this).index();
+			$(this).css('background-color','blue');
+			$(this).data('index',index);
+			$(this).after('<div class="'+select+'-index"><p>blahblah</p></div>')
+		});
+		*/
 	});
 
 
